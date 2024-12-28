8f97f5103a33fc31460a00e613574d7c81142355
mumumusf <106123321+mumumusf@users.noreply.github.com>
2024-12-28 22:18:59 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ class Config {
   constructor() {
     this.baseURL = 'https://nodepay.org';
     this.ipCheckURL = 'https://ipinfo.io/json';
-    this.pingURL = 'http://13.215.134.222/api/network/ping';
+    this.pingURL = 'https://nw.nodepay.org/api/network/ping';
     this.retryInterval = 30000;
     this.sessionURL = 'http://api.nodepay.ai/api/auth/session';
   }
